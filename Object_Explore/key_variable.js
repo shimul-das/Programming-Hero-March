5292f77cfbb17c8e31f13e6c772f76bd7f995cdf
shimul <shimul1322@gmail.com>
2023-03-02 18:59:55 +0600

    36-3 Keys, values, entries, delete, seal, freeze

--- Object_Explore/key_variable.js
+++ Object_Explore/key_variable.js
@@ -10,3 +10,11 @@ const values=Object.values(bottle);
 console.log(values);
 const pair=Object.entries(bottle);
 console.log(bottle);
+// Object.seal(bottle)
+Object.freeze(bottle);
+delete bottle.isClean;
+console.log(bottle);
+//seal means we can not remove any property but we can change exsisting property
+bottle.price=1000;
+console.log(bottle)
+//freeze means we can not change and delete from object.
