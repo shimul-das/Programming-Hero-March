83a28ae09d3124c93943654e2ae9fa5bfa9624b7
shimul <shimul1322@gmail.com>
2023-03-02 18:51:25 +0600

    first Commit

---
+++ Object_Explore/key_variable.js
@@ -0,0 +1,12 @@
+const bottle={
+    color:'red',
+    price:50,
+    isClean:true,
+    capacity:1
+}
+const key=Object.keys(bottle);
+console.log(key);
+const values=Object.values(bottle);
+console.log(values);
+const pair=Object.entries(bottle);
+console.log(bottle);
