dc52747b523197f11c9fe526336fc48a1b79d3b7
shimul <shimul1322@gmail.com>
2023-03-11 19:59:32 +0600

    function baper bank solution done

---
+++ Common error types/dysfunctional-baap-er-bank-main/js/_debug_script.js
@@ -0,0 +1,9 @@
+/*
+1. Go to the live website. And try to reproduce the error.
+2. Check the console to get any type of error.
+3. Read exception throwly.
+4. If the error is new then it search in online.
+5. If a erroe line show and there show page number click the line.
+6. And add a break point. Re run the code (refresh the website). And see wheather you should expected value.
+
+*/
